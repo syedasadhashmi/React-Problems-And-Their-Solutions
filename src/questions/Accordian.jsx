03f21aecbf89d242be56b629ffb02633d024e38a
syedasadhashmi <asadhashmi22@gmail.com>
2025-11-22 04:05:12 +0500

    prev solution of accordian

--- src/questions/Accordian.jsx
+++ src/questions/Accordian.jsx
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 function Accordion({ items }) {
-  const [activeIndex, setActiveIndex] = useState(1);
-
+  const [newAcc, setNewAcc] = useState([]);
   console.log(items);
+  useEffect(() => {
+    const newFunc = () => {
+      const newItems = items?.map((item) => {
+        return { ...item, isOpenAcc: false };
+      });
+      setNewAcc(newItems);
+    };
+    newFunc();
+  }, []);
 
-  const handleToggle = (index) => {
-    console.log(index);
-    if (activeIndex === index) {
-      setActiveIndex(null);
-    } else {
-      setActiveIndex(index);
-    }
+  const accordianHandler = (item) => {
+    setNewAcc((prev) =>
+      prev.map((n) => {
+        if (item.title === n.title) {
+          return { ...n, isOpenAcc: !item.isOpenAcc };
+        } else {
+          return { ...n, isOpenAcc: false };
+        }
+      })
+    );
   };
 
   if (!items || items.length === 0) return <p>No items available</p>;
 
-  return (
+  return newAcc ? (
     <div
       style={{
         border: "1px solid black",
@@ -28,23 +39,43 @@ function Accordion({ items }) {
           "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
       }}
     >
-      {items?.map((item, index) => {
-        return (
-          <div key={index}>
-            <div>
-              <button
-                onClick={() => handleToggle(index)}
-                aria-expanded={activeIndex === index}
-              >
-                {item?.title}
-              </button>
-              {activeIndex === index ? <FaChevronUp /> : <FaChevronDown />}
-            </div>
-            {activeIndex === index && <div>{item?.content}</div>}
+      {newAcc?.map((item) => (
+        <div
+          key={item?.title}
+          style={{
+            border: "1px solid",
+            padding: "2px 10px",
+            margin: "10px 0px",
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.19)",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+            onClick={() => accordianHandler(item)}
+          >
+            <h3>{item.title}</h3>
+            <div>{item?.isOpenAcc ? <FaChevronUp /> : <FaChevronDown />}</div>
           </div>
-        );
-      })}
+          {item?.isOpenAcc && (
+            <div style={{}}>
+              {item?.content ? (
+                <p>{item?.content}</p>
+              ) : (
+                <p>No items available</p>
+              )}
+            </div>
+          )}
+        </div>
+      ))}
     </div>
+  ) : (
+    <p>No items available</p>
   );
 }
 
